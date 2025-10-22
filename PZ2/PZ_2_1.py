310685ded906e9aefda6fafc945bb48cfe867f16
HFOTOD <hhfotod@gmail.com>
2025-10-22 04:04:09 +0300

    Первый коммит

--- PZ2/PZ_2_1.py
+++ PZ2/PZ_2_1.py
@@ -1,1 +1,4 @@
-print("Hello GitHub")
+N = int(input("Введите количество секунд с начала суток: "))
+seconds_in_current_hour = N % 3600
+full_minutes = seconds_in_current_hour // 60
+print(f"Количество полных минут с начала последнего часа: {full_minutes}")
