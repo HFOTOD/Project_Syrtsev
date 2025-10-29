df5f705c9d116c1bdbe892bbaae0ec7e25557e68
HFOTOD <hhfotod@gmail.com>
2025-10-29 16:48:13 +0300

    Update PZ_2_1.py

--- PZ2/PZ_2_1.py
+++ PZ2/PZ_2_1.py
@@ -1,4 +1,10 @@
-N = int(input("Введите количество секунд с начала суток: "))
+while True:
+    N = input("Введи количество секунд с начала суток (только цифры): ")
+    if N.isdigit():
+        N = int(N)
+      break
+    else:
+        print("Пожалуйста вводи только цифры")
 seconds_in_current_hour = N % 3600
 full_minutes = seconds_in_current_hour // 60
-print(f"Количество полных минут с начала последнего часа: {full_minutes}")
+print(f"Полных минут с начала последнего часа: {full_minutes}")
