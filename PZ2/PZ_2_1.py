70edfffa9854b8adf47882494a3353cd9bec79b2
HFOTOD <hhfotod@gmail.com>
2025-10-29 16:51:51 +0300

    Update PZ_2_1.py

--- PZ2/PZ_2_1.py
+++ PZ2/PZ_2_1.py
@@ -2,7 +2,7 @@ while True:
     N = input("Введи количество секунд с начала суток (только цифры): ")
     if N.isdigit():
         N = int(N)
-      break
+        break
     else:
         print("Пожалуйста вводи только цифры")
 seconds_in_current_hour = N % 3600
