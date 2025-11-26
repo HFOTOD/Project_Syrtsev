a50e9d69112e1183cf21c3886a027ff6e5e857fc
HFOTOD <hhfotod@gmail.com>
2025-11-26 17:27:06 +0300

    Update PZ_2_1.py

--- PZ2/PZ_2_1.py
+++ PZ2/PZ_2_1.py
@@ -8,4 +8,4 @@ while True:
 
 seconds_in_current_hour = N % 3600
 full_minutes = seconds_in_current_hour // 60
-print(f"Полных минут с начала последнего часа: {full_minutes}")
+print("Полных минут с начала последнего часа: ", full_minutes)
