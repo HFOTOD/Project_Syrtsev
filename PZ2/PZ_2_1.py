1c4ecdbc58077501207e35fc31ee33ddf44cbb0b
HFOTOD <hhfotod@gmail.com>
2025-11-26 17:08:37 +0300

    Update PZ_2_1.py

--- PZ2/PZ_2_1.py
+++ PZ2/PZ_2_1.py
@@ -1,10 +1,11 @@
 while True:
     try:
-    N = input("Введи количество секунд с начала суток: ")
-    N = int(N)
-    break
-    exept ValueError:
-    print("Ошибка: введите число")
-    seconds_in_current_hour = N % 3600
-    full_minutes = seconds_in_current_hour // 60
-    print(f"Полных минут с начала последнего часа: {full_minutes}")
+        N = input("Введи количество секунд с начала суток: ")
+        N = int(N)
+        break
+    except ValueError:
+        print("Ошибка: введите число")
+
+seconds_in_current_hour = N % 3600
+full_minutes = seconds_in_current_hour // 60
+print(f"Полных минут с начала последнего часа: {full_minutes}")
