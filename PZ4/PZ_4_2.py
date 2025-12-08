b9ca38247742825b3ac0daf70d985fa4c5499885
HFOTOD <hhfotod@gmail.com>
2025-12-09 02:11:40 +0300

    Update PZ_4_2.py

--- PZ4/PZ_4_2.py
+++ PZ4/PZ_4_2.py
@@ -1,21 +1,3 @@
-#Сумма квадратов от N до 2N
-while True:
-    try:
-        N = int(input("Введите целое число N (> 0): "))
-        if N <= 0:
-            print("Ошибка: N должно быть положительным целым числом.")
-            continue
-        break
-    except ValueError:
-        print("Ошибка: введите корректное целое число.")
-
-total_sum = 0
-for i in range(N, 2 * N + 1):
-    total_sum += i * i
-
-print(f"Сумма квадратов от {N} до {2 * N}: {total_sum}")
-
-
 #Банковский вклад с ежемесячным начислением процентов
 while True:
     try:
