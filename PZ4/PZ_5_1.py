512cf80593ec3bbe816de5a88882e066ed316f2e
HFOTOD <hhfotod@gmail.com>
2026-01-16 05:15:56 +0300

    Update PZ_5_1.py

--- PZ4/PZ_5_1.py
+++ PZ4/PZ_5_1.py
@@ -1,1 +1,26 @@
+# Составить функцию решения задачи: из заданного числа вычли сумму его цифр. Из результата вновь вычли сумму его цифр и т. д. Через сколько таких действий получится нуль?
 
+def count_steps_to_zero(n):
+    steps = 0
+    while n > 0:
+        digit_sum = sum(int(digit) for digit in str(n))
+        n -= digit_sum
+        steps += 1
+    return steps
+
+# Ввод числа с проверкой
+number = input("Введите целое положительное число: ")
+while True:
+    try:
+        number = int(number)
+        if number <= 0:
+            print("Число должно быть положительным!")
+            number = input("Введите целое положительное число: ")
+        else:
+            break
+    except ValueError:
+        print("Неверный ввод! Введите целое число.")
+        number = input("Введите целое положительное число: ")
+
+result = count_steps_to_zero(number)
+print(f"Количество шагов до нуля: {result}")
