781f8d21f1b69e20190e9d3f8601838afbe936fe
HFOTOD <hhfotod@gmail.com>
2026-01-16 05:26:20 +0300

    Update PZ_7_1.py

--- PZ4/PZ_7_1.py
+++ PZ4/PZ_7_1.py
@@ -1,1 +1,14 @@
+# Дан символ C и строки S, S0. Перед каждым вхождением символа C в строку S вставить строку S0.
 
+C = input('Введите символ C: ')
+S = input('Введите строку S: ')
+S0 = input('Введите строку S0: ')
+
+result = ""
+for char in S:
+    if char == C:
+        result += S0 + char
+    else:
+        result += char
+
+print('Результат:', result)
