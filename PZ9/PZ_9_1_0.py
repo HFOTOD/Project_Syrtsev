265c28fe21dc958179a9287f2d160df43c38e486
HFOTOD <hhfotod@gmail.com>
2026-02-06 05:11:29 +0300

    9 практичка

--- PZ9/PZ_9_1_0.py
+++ PZ9/PZ_9_1_0.py
@@ -1,1 +1,25 @@
+# Практическое занятие № 9
+# Составление программ с применением множеств
+
+magistr = {'Лермонтов', 'Достоевский', 'Пушкин', 'Тютчев'}
+dom_knigi = {'Толстой', 'Грибоедов', 'Чехов', 'Пушкин'}
+bukmarket = {'Пушкин', 'Достоевский', 'Маяковский'}
+galereya = {'Чехов', 'Тютчев', 'Пушкин'}
+
+required = {'Пушкин', 'Тютчев'}
+result = set()
+
+if required & magistr == required:
+    result.add('Магистр')
+
+if required & dom_knigi == required:
+    result.add('ДомКниги')
+
+if required & bukmarket == required:
+    result.add('БукМаркет')
+
+if required & galereya == required:
+    result.add('Галерея')
+
+print('Магазины, в которых можно приобрести книги Пушкина и Тютчева:', result)
 
