1cb819d29b36a0db6ed105b7e28a772ca4ddf8c6
HFOTOD <hhfotod@gmail.com>
2026-02-20 17:59:23 +0300

    Update PZ_8_1.py

--- PZ8/PZ_8_1.py
+++ PZ8/PZ_8_1.py
@@ -1,1 +1,14 @@
-ку
+sample = {'a': 100, 'b':200, 'c':300}
+for i, key in sample.items():
+    if key == 200:
+        print('norm')
+
+
+import random
+original_dict = {}
+for i in range(7):
+    original_dict[i] = random.randint(0,100)
+print("Исходный словарь:", original_dict)
+del original_dict[0]
+del original_dict[6]
+print("Словарь после удаления первого и последнего элементов:", original_dict)
