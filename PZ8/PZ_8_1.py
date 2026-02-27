f89109d24c8a13d81a0af73fb27f39f30bbcfec6
HFOTOD <hhfotod@gmail.com>
2026-02-27 18:29:04 +0300

    Update PZ_8_1.py

--- PZ8/PZ_8_1.py
+++ PZ8/PZ_8_1.py
@@ -2,13 +2,3 @@ sample = {'a': 100, 'b':200, 'c':300}
 for i, key in sample.items():
     if key == 200:
         print('norm')
-
-original_dict = {}
-for i in range(7):
-    original_dict[i] = i ** 3
-
-print("Исходный словарь:", original_dict)
-del original_dict[0]
-del original_dict[6]
-
-print("Словарь после удаления первого и последнего элементов:", original_dict)
