322294b58ddca421d70610a90fb8cd2b0e992b52
HFOTOD <hhfotod@gmail.com>
2026-04-04 13:59:15 +0300

    10 практичка

--- PZ_10/PZ_10_1.py
+++ PZ_10/PZ_10_1.py
@@ -11,3 +11,30 @@ for i in open('text18-1.txt', encoding='UTF-8'):
 print(end='\n')
 print('Количество строк:', t)
 print('Количество букв "ж":', d)
+
+
+d = 0
+t = 0
+
+for i in open('text18-1.txt', encoding='UTF-8'):
+    print(i, end='')
+    t += 1
+    for j in i:
+        if j == 'ж':
+            d += 1
+
+print('\nКоличество строк:', t)
+print('Количество букв "ж":', d)
+
+
+f1 = open('text18-1.txt', encoding='UTF-8')
+l = f1.readlines()
+f1.close()
+
+# Проверка количества строк
+if len(l) >= 4:
+    l[0], l[3] = l[3], l[0]
+
+f2 = open('text18-2.txt', 'w')
+f2.writelines(l)
+f2.close()
