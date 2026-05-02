9ce666d604fe674af9baf560ea045f6c91cce9a5
HFOTOD <hhfotod@gmail.com>
2026-05-02 04:21:09 +0300

    Update PZ_13_1.py

--- PZ_13/PZ_13_1.py
+++ PZ_13/PZ_13_1.py
@@ -35,7 +35,7 @@ with open('result.txt', 'w', encoding='utf-8') as file:
         file.write(d + '\n')
 
 
-
+дипсик
 
 import re
 
@@ -68,3 +68,37 @@ with open('result.txt', 'w', encoding='utf-8') as file:
         file.write(d + '\n')
 
 print(f"Найдено дат февраля: {len(february_slash)}")
+
+
+переработка чатагпт
+
+import re
+
+with open('dates.txt', 'r', encoding='utf-8') as file:
+    text = file.read()
+
+pattern_dot = re.compile(r"[0-3][0-9]\.[0-1][0-9]\.[1-2][0-9][0-9][0-9]")
+pattern_slash = re.compile(r"[0-3][0-9]/[0-1][0-9]/[1-2][0-9][0-9][0-9]")
+
+dates_dot = pattern_dot.findall(text)
+dates_slash = pattern_slash.findall(text)
+
+count_dot = len(dates_dot)
+count_slash = len(dates_slash)
+
+print("Количество дат с точками:", count_dot)
+print("Количество дат со слешами:", count_slash)
+
+february_slash = []
+
+for date in dates_dot:
+    if re.search(r"\.02\.", date):
+        february_slash.append(date.replace('.', '/'))
+
+for date in dates_slash:
+    if re.search(r"/02/", date):
+        february_slash.append(date)
+
+with open('result.txt', 'w', encoding='utf-8') as file:
+    for d in february_slash:
+        file.write(d + '\n')
