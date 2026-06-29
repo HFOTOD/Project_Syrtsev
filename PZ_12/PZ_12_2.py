9d13da3f7f8b5388e6acfaadd753364e017355b8
HFOTOD <hhfotod@gmail.com>
2026-06-29 12:22:50 +0300

    Update PZ_12_2.py

--- PZ_12/PZ_12_2.py
+++ PZ_12/PZ_12_2.py
@@ -1,6 +1,5 @@
 import random
 
-# 1. Автоматическая генерация матрицы (задаем сами)
 rows = 4
 cols = 4
 
@@ -11,13 +10,11 @@ print(*matrix, sep="\n")
 
 N = int(input(f"\nВведите номер столбца N (от 0 до {cols - 1}): "))
 
-# 2. ЗАДАНИЕ 1: Замена последней строки на 0
 matrix = [
     ([0 for _ in row] if i == len(matrix) - 1 else [x for x in row])
     for i, row in enumerate(matrix)
 ]
 
-# 3. ЗАДАНИЕ 2: Увеличение элементов столбца N в два раза
 matrix = list(map(
     lambda row: [
         (x * 2 if j == N else x)
